74f6081798e28ea433c45c78a1fbe18d0589891c
jay-rdc <delacruz.jross@gmail.com>
2026-01-10 05:13:38 +0800

    style(niri): remove unnecessary variable in session_manager

--- niri/.config/niri/session_manager.sh
+++ niri/.config/niri/session_manager.sh
@@ -5,9 +5,7 @@ option2="󰍃 Logout"
 option3="󰜉 Reboot"
 option4="󰐥 Shutdown"
 
-options="$option1\n$option2\n$option3\n$option4"
-
-selected=$(echo -e "$options" | fuzzel -d --width 20 --lines 4)
+selected=$(echo -e "$option1\n$option2\n$option3\n$option4" | fuzzel --dmenu --width 20 --lines 4)
 
 case $selected in
   "$option1")
