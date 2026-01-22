a5b75713a3b2f097b5b9aa27c9ffc85b8dff814c
jay-rdc <delacruz.jross@gmail.com>
2026-01-23 02:21:38 +0800

    feat(nvim): use updated harpoon branch

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -1,42 +1,41 @@
 return {
-  "theprimeagen/harpoon",
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
   dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {
-    menu = {
-      width = 100,
-    },
-  },
-  config = function(_, opts)
-    require("harpoon").setup(opts)
+  config = function()
+    local harpoon = require("harpoon")
 
-    local mark = require("harpoon.mark")
-    local ui = require("harpoon.ui")
+    harpoon:setup()
 
-    vim.keymap.set("n", "<leader>ma", mark.add_file, { desc = "Harpoon: Mark current buffer" })
-    vim.keymap.set("n", "<leader>mo", ui.toggle_quick_menu, { desc = "Harpoon: Show quick menu" })
+    vim.keymap.set("n", "<leader>ma", function()
+      harpoon:list():add()
+    end, { desc = "Harpoon: Mark current buffer" })
+    vim.keymap.set("n", "<leader>mo", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = "Harpoon: Toggle quick menu" })
 
     vim.keymap.set("n", "<leader>1", function()
-      ui.nav_file(1)
+      harpoon:list():select(1)
     end, { desc = "Harpoon: Switch to mark 1" })
 
     vim.keymap.set("n", "<leader>2", function()
-      ui.nav_file(2)
+      harpoon:list():select(2)
     end, { desc = "Harpoon: Switch to mark 2" })
 
     vim.keymap.set("n", "<leader>3", function()
-      ui.nav_file(3)
+      harpoon:list():select(3)
     end, { desc = "Harpoon: Switch to mark 3" })
 
     vim.keymap.set("n", "<leader>4", function()
-      ui.nav_file(4)
+      harpoon:list():select(4)
     end, { desc = "Harpoon: Switch to mark 4" })
 
     vim.keymap.set("n", "<leader>5", function()
-      ui.nav_file(5)
+      harpoon:list():select(5)
     end, { desc = "Harpoon: Switch to mark 5" })
 
     vim.keymap.set("n", "<leader>6", function()
-      ui.nav_file(6)
+      harpoon:list():select(6)
     end, { desc = "Harpoon: Switch to mark 6" })
   end,
 }
