b9fa5f21a212342dcb4b43b246f356c3fa225bb9
jay-rdc <delacruz.jross@gmail.com>
2026-04-24 21:56:08 +0800

    feat(nvim): add autocmd that deletes empty and unmodified buffers

---
+++ nvim/.config/nvim/lua/config/autocmd.lua
@@ -0,0 +1,17 @@
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    local bufs = vim.api.nvim_list_bufs()
+    if #bufs > 1 then
+      local prev_buf = vim.fn.bufnr("#")
+      -- Check if the previous buffer was unnamed, empty, and unmodified
+      if
+        prev_buf > 0
+        and vim.api.nvim_buf_get_name(prev_buf) == ""
+        and vim.api.nvim_buf_get_option(prev_buf, "buftype") == ""
+        and not vim.api.nvim_buf_get_option(prev_buf, "modified")
+      then
+        vim.api.nvim_buf_delete(prev_buf, { force = true })
+      end
+    end
+  end,
+})
--- nvim/.config/nvim/lua/config/init.lua
+++ nvim/.config/nvim/lua/config/init.lua
@@ -1,5 +1,6 @@
+require("vim._core.ui2").enable({})
+
 require("config.remap")
 require("config.options")
 require("config.pack")
-
-require("vim._core.ui2").enable({})
+require("config.autocmd")
