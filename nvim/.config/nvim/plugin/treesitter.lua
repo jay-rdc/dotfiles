107647eaf823464ec2963184ad52c4e6c2c5159f
jay-rdc <delacruz.jross@gmail.com>
2026-04-26 03:25:59 +0800

    refactor(nvim): remove unncessary logic in treesitter TSUpdate autocmd

--- nvim/.config/nvim/plugin/treesitter.lua
+++ nvim/.config/nvim/plugin/treesitter.lua
@@ -25,9 +25,6 @@ vim.api.nvim_create_autocmd("PackChanged", {
   callback = function(ev)
     local name, kind = ev.data.spec.name, ev.data.kind
     if name == "nvim-treesitter" and kind == "update" then
-      if not ev.data.active then
-        vim.cmd.packadd("nvim-treesitter")
-      end
       vim.cmd("TSUpdate")
     end
   end,
